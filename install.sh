eb88f06d00dc508810ff595c98bea82bf62899e9
sanander0 <ivasan082@gmail.com>
2026-03-28 20:12:26 +0300

    modified:   install.sh

--- install.sh
+++ install.sh
@@ -3,3 +3,14 @@
 sudo xbps-install -S dbus elogind seatd
 sudo xbps-install -S mesa-dri
 sudo xbps-install -S river Waybar wofi mako
+#sudo xbps-install -S greetd
+
+cp -r river/ ~/.config/
+
+
+
+sudo ln -s /etc/sv/elogind /var/service/
+
+sudo ln -s /etc/sv/dbus /var/service/
+#sudo ln -s /etc/sv/greetd /var/service/
+sudo ln -s /etc/sv/seatd /var/service/
